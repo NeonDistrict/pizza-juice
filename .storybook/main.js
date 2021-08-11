eda22b8b7f235ef4155e94170185c92fc6387a88
gabrieldemian <gabrielgcr45@gmail.com>
2021-08-11 11:35:58 -0300

    consegui porra

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,10 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    'storybook-addon-styled-component-theme/dist/preset'
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  core: {
-    builder: 'storybook-builder-vite'
-  }
-};
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
