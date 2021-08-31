74936a357ce06e1f78a22dccdf7c5e4d9d182b93
Miguel Nascimento <miguelgomes13@live.com>
2021-08-31 15:40:49 -0300

    chore: test for export components

---
+++ index.ts
@@ -0,0 +1,8 @@
+import { Breadcrumb } from 'src/components/Breadcrumbs';
+import { Button } from 'src/components/Button';
+import { Card } from 'src/components/Card';
+import { Modal } from 'src/components/Modal';
+import { Steps } from 'src/components/Steps';
+import { Tab } from 'src/components/Tabs';
+
+export { Breadcrumb, Button, Card, Modal, Steps, Tab };
