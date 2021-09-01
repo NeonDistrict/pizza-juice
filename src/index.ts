8a68607a94f0ead105b9d4843b7904123c24de36
Miguel Nascimento <miguelgomes13@live.com>
2021-09-01 12:30:26 -0300

    chore: export theme provider and theme

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import { ComponentProvider } from './context/ComponentProvider';
 import { Breadcrumb } from './components/Breadcrumbs';
 import { Button } from './components/Button';
 import { Card } from './components/Card';
 import { Modal } from './components/Modal';
 import { Steps } from './components/Steps';
 import { Tab } from './components/Tabs';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
 
-export { Breadcrumb, Button, Card, Modal, Steps, Tab, ComponentProvider };
+export {
+  Breadcrumb,
+  Button,
+  Card,
+  Modal,
+  Steps,
+  Tab,
+  ThemeProvider as ComponentProvider,
+  theme as componentTheme
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
 import { App } from './App';
-import { GlobalStyles } from './styles/global-styles';
-import { theme } from './styles/theme';
-import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-      <GlobalStyles />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
