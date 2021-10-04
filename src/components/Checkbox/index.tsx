5b4881ddf76e7a82d5004e3a82297159f5f19e91
Vinicius Vicentini <viniciusvicentini@live.com>
2021-10-04 12:35:35 -0300

    feat: add forwardRef / comments

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -7,42 +7,51 @@ import { AiOutlineCheck } from 'react-icons/ai';
 import { Wrapper, Label, CheckboxInput, CheckboxIndicator } from './styles';
 
 export type CheckboxProps = {
-  children: React.ReactNode;
+  /**
+   * Checkbox size, default value `md`
+   */
   size?: 'sm' | 'md' | 'lg';
+  /**
+   * Checkbox text
+   */
+  children: React.ReactNode;
 } & CheckboxBase.CheckboxProps;
 
-const Checkbox = ({
-  defaultChecked,
-  value,
-  size = 'md',
-  children,
-  ...props
-}: CheckboxProps) => {
-  const iconSizes = {
-    sm: '10',
-    md: '12',
-    lg: '15'
-  };
+/**
+ * Checkbox component made with Radix-UI
+ *
+ * To check all props
+ * @see https://www.radix-ui.com/docs/primitives/components/checkbox
+ */
+const Checkbox = React.forwardRef<HTMLButtonElement, CheckboxProps>(
+  ({ defaultChecked, value, size = 'md', children, ...props }, ref) => {
+    const iconSizes = {
+      sm: '10',
+      md: '12',
+      lg: '15'
+    };
 
-  return (
-    <Wrapper>
-      <CheckboxInput
-        id={`checkbox-${value}`}
-        value={value}
-        size={size}
-        defaultChecked={defaultChecked}
-        {...props}
-      >
-        <CheckboxIndicator>
-          <AiOutlineCheck size={iconSizes[size]} />
-        </CheckboxIndicator>
-      </CheckboxInput>
+    return (
+      <Wrapper>
+        <CheckboxInput
+          ref={ref}
+          id={`checkbox-${value}`}
+          value={value}
+          size={size}
+          defaultChecked={defaultChecked}
+          {...props}
+        >
+          <CheckboxIndicator>
+            <AiOutlineCheck size={iconSizes[size]} />
+          </CheckboxIndicator>
+        </CheckboxInput>
 
-      <Label htmlFor={`checkbox-${value}`} size={size}>
-        {children}
-      </Label>
-    </Wrapper>
-  );
-};
+        <Label htmlFor={`checkbox-${value}`} size={size}>
+          {children}
+        </Label>
+      </Wrapper>
+    );
+  }
+);
 
 export default Checkbox;
