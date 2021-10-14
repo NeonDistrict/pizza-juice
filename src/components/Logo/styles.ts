37ba36ba98c913265544b5ee8a2b1b24551165a1
Vinicius Vicentini <viniciusvicentini@live.com>
2021-10-14 16:12:25 -0300

    feat: logo component

---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,39 @@
+import styled, { css, DefaultTheme } from 'styled-components';
+
+import { LogoProps } from '.';
+
+const variants = (theme: DefaultTheme) => ({
+  success: css`
+    color: ${theme.colors.white};
+    background: ${theme.colors.green['100']};
+  `,
+  warning: css`
+    color: ${theme.colors.grey[500]};
+    background: ${theme.colors.yellow['100']};
+  `,
+  danger: css`
+    color: ${theme.colors.white};
+    background: ${theme.colors.red['100']};
+  `
+});
+
+export const LogoStyles = styled.div<Pick<LogoProps, 'variant'>>`
+  ${({ theme, variant }) => css`
+    width: 105px;
+    height: 29px;
+    display: flex;
+    align-items: center;
+
+    ${variant && variants(theme)[variant]}
+  `}
+`;
+
+export const Text = styled.div`
+  ${({ theme }) => css`
+    padding: 4px 0px 4px 8px;
+    user-select: none;
+    text-transform: uppercase;
+    line-height: 150%;
+    font-size: ${theme.fontSizes.sm};
+  `}
+`;
