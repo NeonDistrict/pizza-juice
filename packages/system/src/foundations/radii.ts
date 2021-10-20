71c2ebb670d6de070686105e4646b926431966d5
Vinicius Vicentini <viniciusvicentini@live.com>
2021-10-20 09:46:38 -0300

    chore: move px to rem, fix radii

--- packages/system/src/foundations/radii.ts
+++ packages/system/src/foundations/radii.ts
@@ -1,10 +1,10 @@
 export type Radii = typeof radii;
 
 const radii = {
-  full: 9999,
+  full: '9999px',
   half: '50%',
-  sm: 5,
-  md: 12
+  sm: '5px',
+  md: '12px'
 };
 
 export default radii;
--- packages/system/src/foundations/space.ts
+++ packages/system/src/foundations/space.ts
@@ -1,26 +1,28 @@
 export type Space = typeof space;
 
+const convertToREM = (value: number, base = 16) => `${value / base}rem`;
+
 const space = {
-  1: '4px',
-  2: '8px',
-  3: '12px',
-  4: '16px',
-  5: '20px',
-  6: '24px',
-  7: '28px',
-  8: '32px',
-  9: '36px',
-  10: '40px',
-  11: '44px',
-  12: '48px',
-  13: '52px',
-  14: '56px',
-  15: '60px',
-  16: '64px',
-  17: '68px',
-  18: '72px',
-  19: '76px',
-  20: '80px'
+  1: convertToREM(4),
+  2: convertToREM(8),
+  3: convertToREM(12),
+  4: convertToREM(16),
+  5: convertToREM(20),
+  6: convertToREM(24),
+  7: convertToREM(28),
+  8: convertToREM(32),
+  9: convertToREM(36),
+  10: convertToREM(40),
+  11: convertToREM(44),
+  12: convertToREM(48),
+  13: convertToREM(52),
+  14: convertToREM(56),
+  15: convertToREM(60),
+  16: convertToREM(64),
+  17: convertToREM(68),
+  18: convertToREM(72),
+  19: convertToREM(76),
+  20: convertToREM(80)
 };
 
 export default space;
