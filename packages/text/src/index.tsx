13583655c1a266d78a0c5a534ed074f32dd2ad23
Vinicius Vicentini <viniciusvicentini@live.com>
2021-10-25 12:04:08 -0300

    feat: text primitive component

--- packages/text/src/index.tsx
+++ packages/text/src/index.tsx
@@ -1,39 +1,74 @@
-import { ComponentProps } from 'react';
+import { HTMLAttributes } from 'react';
 
-import styled from '@nd-ui/system';
+import styled, { DefaultTheme } from '@nd-ui/system';
 
-export type TextProps = ComponentProps<typeof Text>;
+export type TextProps = {
+  size?: keyof DefaultTheme['fontSizes'];
+} & HTMLAttributes<HTMLSpanElement>;
 
-export const Text = styled('span', {
-  lineHeight: '1',
-  margin: '0',
-  fontWeight: '$normal',
-  display: 'block',
-  color: '$white',
-
-  /**
-   * Variants
-   */
-  variants: {
-    size: {
-      '3xs': {
-        fontSize: '$3xs'
-      },
-      '2xs': {
-        fontSize: '$2xs'
-      },
-      xs: {
-        fontSize: '$xs'
-      },
-      sm: {
-        fontSize: '$sm'
-      },
-      md: {
-        fontSize: '$md'
-      },
-      lg: {
-        fontSize: '$lg'
-      }
+const variants = (theme: DefaultTheme) => ({
+  size: {
+    xs: {
+      fontSize: theme.fontSizes.xs
+    },
+    sm: {
+      fontSize: theme.fontSizes.sm
+    },
+    md: {
+      fontSize: theme.fontSizes.md
+    },
+    lg: {
+      fontSize: theme.fontSizes.lg
+    },
+    xl: {
+      fontSize: theme.fontSizes.xl
+    },
+    '2xl': {
+      fontSize: theme.fontSizes['2xl']
+    },
+    '3xl': {
+      fontSize: theme.fontSizes['3xl']
+    },
+    '4xl': {
+      fontSize: theme.fontSizes['4xl']
     }
   }
 });
+
+const Text = styled.span<TextProps>(({ theme, size }) => ({
+  lineHeight: '1',
+  margin: '0',
+  fontWeight: theme.fontWeights.md,
+  display: 'block',
+  color: theme.colors.white,
+
+  ...(size && variants(theme).size[size])
+}));
+
+export default Text;
+
+/**
+ * Variants
+ */
+// variants: {
+//   size: {
+//     '3xs': {
+//       fontSize: '$3xs'
+//     },
+//     '2xs': {
+//       fontSize: '$2xs'
+//     },
+//     xs: {
+//       fontSize: '$xs'
+//     },
+//     sm: {
+//       fontSize: '$sm'
+//     },
+//     md: {
+//       fontSize: '$md'
+//     },
+//     lg: {
+//       fontSize: '$lg'
+//     }
+//   }
+// }
