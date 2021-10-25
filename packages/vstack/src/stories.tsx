92eb3dfb11ebc2ebba185a3c51250c4e314889bb
Vinicius Vicentini <viniciusvicentini@live.com>
2021-10-25 12:03:50 -0300

    feat: vstack primitive component

---
+++ packages/vstack/src/stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import VStack, { VStackProps } from '.';
+
+import Box from '@nd-ui/box';
+
+export default {
+  title: 'Components/Layout/VStack',
+  component: VStack
+} as Meta;
+
+export const Default: Story<VStackProps> = args => (
+  <VStack {...args} spacing={5}>
+    <Box>123</Box>
+
+    <Box>123</Box>
+
+    <Box>123</Box>
+  </VStack>
+);
+
+Default.args = {};
