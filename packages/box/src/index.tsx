3d7bf52a6cdcdaf6db3812bfdace49316759967c
Vinicius Vicentini <viniciusvicentini@live.com>
2021-10-25 12:03:27 -0300

    feat: box primitive component

--- packages/box/src/index.tsx
+++ packages/box/src/index.tsx
@@ -4,7 +4,7 @@ import styled from '@nd-ui/system';
 
 export type BoxProps = HTMLAttributes<HTMLDivElement>;
 
-const Box = styled.div({
+const Box = styled.div<BoxProps>({
   display: 'block'
 });
 
---
+++ packages/box/src/stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import Box, { BoxProps } from '.';
+
+export default {
+  title: 'Components/Layout/Box',
+  component: Box
+} as Meta;
+
+export const Default: Story<BoxProps> = args => <Box {...args} />;
+
+Default.args = {};
