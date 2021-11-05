f4ac5ba079fae77084a5ce572d60b1cb0ecc27d8
Vinicius Vicentini <viniciusvicentini@live.com>
2021-11-05 10:50:20 -0300

    chore: update exports

--- index.ts
+++ index.ts
@@ -21,12 +21,23 @@ export * from './src/components/radio';
 export * from './src/components/select';
 export * from './src/components/steps';
 export * from './src/components/tab';
-export * as Theme from './src/theme';
-export * as Toggle from './src/components/toggle';
-export * as Tag from './src/components/tag';
-export * as Textarea from './src/components/textarea';
-// export * from './packages/heading';
-export * as PageHeading from './src/components/page-heading';
+export * from './src/theme';
+export * from './src/components/toggle';
+export * from './src/components/tag';
+export * from './src/components/textarea';
+export * from './src/components/heading';
+export * from './src/components/page-heading';
 export * from './src/components/pagination';
-// // export * as Text from './packages/text';
-// // export * as Vstack from './packages/vstack';
+export * from './src/components/text';
+export * from './src/components/stack';
+
+export {
+  config,
+  css,
+  globalCss,
+  styled,
+  getCssText,
+  VariantProps,
+  CSS,
+  ComponentProps
+} from './src/system';
