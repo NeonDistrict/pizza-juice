6ae8825818c8e1fc415490a566d9880d8f76a394
Miguel Nascimento <miguelgomes13@live.com>
2021-12-02 15:13:27 -0300

    feat: improved flex primitive

--- src/components/flex/index.tsx
+++ src/components/flex/index.tsx
@@ -15,4 +15,134 @@ import { styled } from '../../system';
  */
 export const Flex = styled('div', {
   d: 'flex',
+
+  variants: {
+    direction: {
+      row: {
+        flexDirection: 'row',
+      },
+      column: {
+        flexDirection: 'column',
+      },
+      rowReverse: {
+        flexDirection: 'row-reverse',
+      },
+      columnReverse: {
+        flexDirection: 'column-reverse',
+      },
+    },
+    align: {
+      start: {
+        alignItems: 'flex-start',
+      },
+      center: {
+        alignItems: 'center',
+      },
+      end: {
+        alignItems: 'flex-end',
+      },
+      stretch: {
+        alignItems: 'stretch',
+      },
+      baseline: {
+        alignItems: 'baseline',
+      },
+    },
+    justify: {
+      start: {
+        justifyContent: 'flex-start',
+      },
+      center: {
+        justifyContent: 'center',
+      },
+      end: {
+        justifyContent: 'flex-end',
+      },
+      between: {
+        justifyContent: 'space-between',
+      },
+      around: {
+        justifyContent: 'space-around',
+      },
+    },
+    wrap: {
+      noWrap: {
+        flexWrap: 'nowrap',
+      },
+      wrap: {
+        flexWrap: 'wrap',
+      },
+      wrapReverse: {
+        flexWrap: 'wrap-reverse',
+      },
+    },
+    gap: {
+      1: {
+        gap: '$1',
+      },
+      2: {
+        gap: '$2',
+      },
+      3: {
+        gap: '$3',
+      },
+      4: {
+        gap: '$4',
+      },
+      5: {
+        gap: '$5',
+      },
+      6: {
+        gap: '$6',
+      },
+      7: {
+        gap: '$7',
+      },
+      8: {
+        gap: '$8',
+      },
+      9: {
+        gap: '$9',
+      },
+      10: {
+        gap: '$10',
+      },
+      11: {
+        gap: '$11',
+      },
+      12: {
+        gap: '$12',
+      },
+      13: {
+        gap: '$13',
+      },
+      14: {
+        gap: '$14',
+      },
+      15: {
+        gap: '$15',
+      },
+      16: {
+        gap: '$16',
+      },
+      17: {
+        gap: '$17',
+      },
+      18: {
+        gap: '$18',
+      },
+      19: {
+        gap: '$19',
+      },
+      20: {
+        gap: '$20',
+      },
+    },
+  },
+  defaultVariants: {
+    direction: 'row',
+    align: 'stretch',
+    justify: 'start',
+    wrap: 'noWrap',
+  },
 });
