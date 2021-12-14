d4115e22d42660afee9f9879792d3e3fb1d2c66e
Vinicius Vicentini <viniciusvicentini@live.com>
2021-12-14 12:55:22 -0300

    chore: add height variable

--- src/components/drawer/styles.ts
+++ src/components/drawer/styles.ts
@@ -3,9 +3,10 @@ import { styled } from '../../system';
 export const Wrapper = styled('div', {
   // variables
   $$width: '360px',
+  $$height: '100%',
 
   w: '$$width',
-  h: '100%',
+  h: '$$height',
   color: '$white',
   bg: '$grey-850',
 });
