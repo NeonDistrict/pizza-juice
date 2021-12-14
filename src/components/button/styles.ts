c6b838c9ecccec2ce198371f3043ea217c826cb1
Miguel Nascimento <miguelgomes13@live.com>
2021-12-14 12:00:35 -0300

    feat: new button

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -14,6 +14,7 @@ export const Button = styled('button', {
 
   // base styles
   py: '$2',
+  px: '$6',
   fontWeight: 600,
   textTransform: 'uppercase',
   color: '$white',
@@ -28,99 +29,60 @@ export const Button = styled('button', {
     cursor: 'pointer',
   },
 
+  '&:focus': {
+    borderColor: '$teal-500',
+  },
+
+  '&:disabled': {
+    $$disabledBg: '$colors$grey-600',
+    cursor: 'not-allowed',
+    color: '$grey-700',
+    '&:hover': {
+      color: '$grey-700',
+    },
+    '&:active': {
+      color: '$grey-700',
+    },
+  },
+
   /**
    * Variants
    */
   variants: {
     /**
-     * Variant variant
+     * Variants solid variant
      */
     variant: {
       primary: {
-        bg: '$pink-500',
-
-        '&:hover': {
-          bg: '$pink-600',
-        },
-        '&:focus': {
-          borderColor: '$teal-500',
-        },
-        '&:active': {
-          bg: '$pink-700',
-        },
-        '&:disabled': {
-          bg: '$grey-400',
-          color: '$grey-600',
-        },
+        $$bgSolid: '$colors$pink-500',
+        $$bgHover: '$colors$pink-600',
+        $$bgActive: '$colors$pink-700',
       },
       secondary: {
-        bg: '$grey-700',
-
-        '&:hover': {
-          bg: '$grey-800',
-        },
-        '&:focus': {
-          bg: '$grey-800',
-          borderColor: '$teal-500',
-        },
-        '&:active': {
-          bg: '$grey-800',
-        },
-        '&:disabled': {
-          bg: '$grey-400',
-          color: '$grey-600',
-        },
+        $$bgSolid: '$colors$grey-700',
+        $$bgHover: '$colors$grey-800',
+        $$bgActive: '$colors$grey-850',
       },
       destructive: {
-        color: '$red-500',
-        borderColor: '$red-500',
-
-        '&:hover': {
-          bg: '$red-900',
-        },
-        '&:focus': {
-          borderColor: '$teal-500',
-        },
-        '&:active': {
-          bg: '$red-900',
-        },
-        '&:disabled': {
-          color: '$grey-600',
-          borderColor: '$grey-400',
-          bg: '$grey-8000',
-        },
+        color: '$black',
+        $$bgSolid: '$colors$red-500',
+        $$bgHover: '$colors$red-550',
+        $$bgActive: '$colors$red-600',
       },
-      outline: {
-        borderColor: '$pink-500',
-
-        '&:focus': {
-          borderColor: '$teal-500',
-        },
-        '&:disabled': {
-          color: '$grey-600',
-          borderColor: '$grey-400',
-        },
+      warning: {
+        color: '$black',
+        $$bgSolid: '$colors$yellow-500',
+        $$bgHover: '$colors$yellow-550',
+        $$bgActive: '$colors$yellow-600',
       },
-      naked: {
-        color: '$pink-500',
-
-        '&:hover': {
-          color: '$pink-600',
-          bg: '$red-900',
-        },
-        '&:focus': {
-          borderColor: '$teal-500',
-        },
-        '&:active': {
-          color: '$pink-600',
-          bg: 'transparent',
-        },
-        '&:disabled': {
-          color: '$grey-600',
-          bg: 'transparent',
-        },
+      success: {
+        color: '$black',
+        $$bgSolid: '$colors$green-500',
+        $$bgHover: '$colors$green-550',
+        $$bgActive: '$colors$green-600',
       },
     },
+
     /**
      * Shape types
      */
@@ -132,6 +94,71 @@ export const Button = styled('button', {
         br: 0,
       },
     },
+
+    /**
+     * Style variant
+     */
+    style: {
+      solid: {
+        '&:disabled': {
+          bg: '$$disabledBg',
+          '&:hover': {
+            bg: '$$disabledBg',
+          },
+          '&:active': {
+            bg: '$$disabledBg',
+          },
+        },
+        bg: '$$bgSolid',
+
+        '&:hover': {
+          bg: '$$bgHover',
+        },
+        '&:active': {
+          bg: '$$bgActive',
+        },
+      },
+      naked: {
+        '&:disabled': {
+          borderColor: 'transparent',
+          '&:hover': {
+            borderColor: 'transparent',
+            bg: 'transparent',
+          },
+          '&:active': {
+            borderColor: 'transparent',
+            bg: 'transparent',
+          },
+        },
+        bg: 'transparent',
+        '&:hover': {
+          bg: '$$bgHover',
+        },
+        '&:active': {
+          bg: '$$bgActive',
+        },
+      },
+      outlined: {
+        '&:disabled': {
+          borderColor: '$$disabledBg',
+          '&:hover': {
+            borderColor: '$$disabledBg',
+          },
+          '&:active': {
+            borderColor: '$$disabledBg',
+          },
+        },
+        bg: 'transparent',
+        borderColor: '$$bgSolid',
+        '&:hover': {
+          borderColor: '$$bgHover',
+        },
+        '&:active': {
+          borderColor: '$$bgActive',
+        },
+      },
+    },
+
     /**
      * Size types
      */
@@ -149,6 +176,7 @@ export const Button = styled('button', {
         px: '$6',
       },
     },
+
     /**
      * Loading variant
      */
@@ -162,6 +190,7 @@ export const Button = styled('button', {
         },
       },
     },
+
     /**
      * Fluid variant
      */
@@ -177,22 +206,123 @@ export const Button = styled('button', {
      * If the component does'nt have children, but have a icon
      */
     onlyIcon: {
-      true: {},
+      true: {
+        p: '$3',
+      },
     },
   },
   compoundVariants: [
+    // Primary styles variant
     {
-      size: 'md',
-      onlyIcon: 'true',
+      variant: 'primary',
+      style: 'outlined',
+      css: {},
+    },
+    {
+      variant: 'primary',
+      style: 'naked',
       css: {
-        p: '$2',
+        color: '$pink-500',
+        '&:hover': {
+          color: '$pink-600',
+          $$bgHover: '#280D19',
+        },
+        '&:active': {
+          color: '$pink-600',
+          $$bgActive: '#280D19',
+        },
+      },
+    },
+    // Secondary styles variant
+    {
+      variant: 'secondary',
+      style: 'outlined',
+      css: {},
+    },
+    {
+      variant: 'secondary',
+      style: 'naked',
+      css: {},
+    },
+    // Destructive styles variant
+    {
+      variant: 'destructive',
+      style: 'outlined',
+      css: {
+        color: '$$bgSolid',
+        '&:hover': {
+          color: '$$bgHover',
+        },
+        '&:active': {
+          color: '$$bgActive',
+        },
       },
     },
     {
-      size: 'sm',
-      onlyIcon: 'true',
+      variant: 'destructive',
+      style: 'naked',
       css: {
-        p: '$2',
+        color: '$red-500',
+        '&:hover': {
+          color: '$black',
+        },
+        '&:active': {
+          color: '$black',
+        },
+      },
+    },
+    // Warning styles variant
+    {
+      variant: 'warning',
+      style: 'outlined',
+      css: {
+        color: '$$bgSolid',
+        '&:hover': {
+          color: '$$bgHover',
+        },
+        '&:active': {
+          color: '$$bgActive',
+        },
+      },
+    },
+    {
+      variant: 'warning',
+      style: 'naked',
+      css: {
+        color: '$yellow-500',
+        '&:hover': {
+          color: '$black',
+        },
+        '&:active': {
+          color: '$black',
+        },
+      },
+    },
+    // Success styles variant
+    {
+      variant: 'success',
+      style: 'outlined',
+      css: {
+        color: '$$bgSolid',
+        '&:hover': {
+          color: '$$bgHover',
+        },
+        '&:active': {
+          color: '$$bgActive',
+        },
+      },
+    },
+    {
+      variant: 'success',
+      style: 'naked',
+      css: {
+        color: '$green-500',
+        '&:hover': {
+          color: '$black',
+        },
+        '&:active': {
+          color: '$black',
+        },
       },
     },
   ],
@@ -203,7 +333,7 @@ export const Button = styled('button', {
   defaultVariants: {
     variant: 'primary',
     shape: 'squared',
-    size: 'md',
+    style: 'solid',
     loading: false,
   },
 });
