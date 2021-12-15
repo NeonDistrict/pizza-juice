7eade8343b554a9670d26c7e4ff0fecb4cefd02e
Miguel Nascimento <miguelgomes13@live.com>
2021-12-15 17:32:11 -0300

    feat: alert component

---
+++ src/components/alert/icons/destructive.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { HTMLAttributes } from 'react';
+
+type DestructiveIconProps = HTMLAttributes<HTMLOrSVGElement>;
+
+export const DestructiveIcon = ({ ...props }: DestructiveIconProps) => (
+  <svg
+    stroke="currentColor"
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 24 24"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C21.9939 17.5203 17.5203 21.9939 12 22ZM11 15V17H13V15H11ZM11 7V13H13V7H11Z"
+      fill="currentColor"
+    />
+  </svg>
+);
---
+++ src/components/alert/icons/warning.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { HTMLAttributes } from 'react';
+
+type WarningIconProps = HTMLAttributes<HTMLOrSVGElement>;
+
+export const WarningIcon = ({ ...props }: WarningIconProps) => (
+  <svg
+    stroke="currentColor"
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 24 24"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M21.266 20.998H2.73301C2.37575 20.998 2.04563 20.8074 1.867 20.498C1.68837 20.1886 1.68838 19.8074 1.86701 19.498L11.133 3.49799C11.3118 3.1891 11.6416 2.9989 11.9985 2.9989C12.3554 2.9989 12.6852 3.1891 12.864 3.49799L22.13 19.498C22.3085 19.8072 22.3086 20.1882 22.1303 20.4975C21.9519 20.8069 21.6221 20.9976 21.265 20.998H21.266ZM12 5.99799L4.46901 18.998H19.533L12 5.99799ZM12.995 14.999H10.995V9.99799H12.995V14.999Z"
+      fill="currentColor"
+    />
+    <path d="M11 16H13V18H11V16Z" fill="currentColor" />
+  </svg>
+);
