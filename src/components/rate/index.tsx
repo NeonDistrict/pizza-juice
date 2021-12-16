c200e869cd9e7e12eac8cad5f1d023f16e15cef6
Vinicius Vicentini <viniciusvicentini@live.com>
2021-12-16 17:44:14 -0300

    feat: new component: Rate

---
+++ src/components/rate/index.tsx
@@ -0,0 +1,64 @@
+import React, { HTMLAttributes } from 'react';
+
+import { CSS } from '../../system';
+
+import { forwardRef } from '../../utils/forwardRef';
+
+import { Stack } from '../stack';
+import { VisuallyHidden } from '../visually-hidden';
+
+import { StarIcon } from './icon';
+
+import * as S from './styles';
+
+export type RateProps = {
+  /**
+   * Total number of selected stars
+   */
+  value: number;
+  /**
+   * CSS properties
+   */
+  css?: CSS;
+} & HTMLAttributes<HTMLDivElement>;
+
+/**
+ * Rate component
+ *
+ * @description Ratings provide insight regarding others' opinions and experiences.
+ */
+export const Rate = forwardRef<RateProps, 'div'>((props) => {
+  const { value, ...rest } = props;
+
+  if (value < 0 || value > 5) {
+    throw new Error('Rate value must be between 0 and 5');
+  }
+
+  const totalStars = [...Array(5)];
+
+  // pluralize the label
+  const pluralStar = value > 1 ? 'stars' : 'star';
+
+  // message to be read by screen readers
+  const a11yMessage = `${value} out of 5 ${pluralStar}`;
+
+  return (
+    <Stack gap="4" aria-valuenow={value} {...rest}>
+      {totalStars.map((_, index) => {
+        index += 1;
+        return (
+          <S.StarWrapper
+            key={index}
+            className={index <= value ? 'active' : undefined}
+          >
+            <StarIcon />
+
+            <VisuallyHidden>{`${index} star`}</VisuallyHidden>
+          </S.StarWrapper>
+        );
+      })}
+
+      <VisuallyHidden>{a11yMessage}</VisuallyHidden>
+    </Stack>
+  );
+});
